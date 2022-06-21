c4ed1b148d2c17e61f517d558d307e1a0e21f3ca
OlegFilippoff <5157175@list.ru>
2022-06-21 11:02:07 +0300

    added src files

--- src/test/java/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -1,4 +1,22 @@
+package ru.netology.statistic;
+
+
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class StatisticServiceTest {
-  
+    @Test
+    void findMax() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 15, 12};
+        long expected = 15;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
 }
